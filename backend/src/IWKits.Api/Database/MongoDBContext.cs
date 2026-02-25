7b52e4cb50c9e1426e1967e74a0c453ea635a0aa
QDestTM <dimag4681@gmail.com>
2026-02-25 17:03:25 +0200

    feat(iwkits-api): implement compound indexes generation for database

--- backend/src/IWKits.Api/Database/MongoDBContext.cs
+++ backend/src/IWKits.Api/Database/MongoDBContext.cs
@@ -1,8 +1,11 @@
 namespace IWKits.Api;
 
 // Namespaces used by this file
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using MongoDB.Driver;
+using System.Linq;
+using System;
 
 // Main content of the file
 public sealed class MongoDBContext : IMongoDBContext
@@ -27,7 +30,39 @@ public sealed class MongoDBContext : IMongoDBContext
 
 	public async Task ConfigureDatabaseAsync()
 	{
-		
+		await ConfigureOrdersCollectionIndexesAsync();
+	}
+
+	// ------------------------------------------------------------------------------------------------------<
+
+	private async Task ConfigureOrdersCollectionIndexesAsync()
+	{
+		var indexKeysDefBuilder = Builders<OrderInfo>.IndexKeys;
+
+		// Define the list of fields that will support sorting
+		Expression<Func<OrderInfo, object>>[] fieldsToIndex =
+		[
+			x => x.Timestamp,
+			x => x.CompositeTaxRate,
+			x => x.Subtotal,
+			x => x.TaxAmount,
+			x => x.TotalAmount
+		];
+
+		// Project each field into a Compound Index model
+		var indexDefinitions = fieldsToIndex.Select((field) =>
+		{
+			var keys = indexKeysDefBuilder.Combine
+			(
+				indexKeysDefBuilder.Descending(field),
+				indexKeysDefBuilder.Ascending(x => x.Id)
+			);
+
+			return new CreateIndexModel<OrderInfo>(keys);
+		});
+
+		// Execute async index creation with created index definitions
+		await Orders.Indexes.CreateManyAsync(indexDefinitions);
 	}
 
 	// ------------------------------------------------------------------------------------------------------<
