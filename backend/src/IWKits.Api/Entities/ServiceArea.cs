265b855374153d1cd546d62c536e61d3c621dda8
QDestTM <dimag4681@gmail.com>
2026-02-27 14:56:05 +0200

    feat(iwkits-api): implement ServiceArea entity

---
+++ backend/src/IWKits.Api/Entities/ServiceArea.cs
@@ -0,0 +1,29 @@
+namespace IWKits.Api.Entities;
+
+// Namespaces used by this file
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Bson;
+
+// Main content of the file
+[BsonIgnoreExtraElements]
+public sealed record ServiceArea
+{
+	// ^ ----------------------------------------------------------------------------------------------------<
+
+	[BsonElement("state_id")]
+	public string StateId { get; init; } = string.Empty;
+
+	[BsonElement("boundary")]
+	public GeoJsonMultiPolygon<GeoJson2DGeographicCoordinates> Boundary { get; init; }
+		= GeoJson.MultiPolygon(
+			GeoJson.PolygonCoordinates(
+				GeoJson.Geographic(0.0d, 0.0d),
+				GeoJson.Geographic(1.0d, 0.0d),
+				GeoJson.Geographic(0.0d, 1.0d),
+				GeoJson.Geographic(0.0d, 0.0d)
+			)
+		);
+
+	// ------------------------------------------------------------------------------------------------------<
+}
