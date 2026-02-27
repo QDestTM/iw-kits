85b28cbbfd1d8d627172b3cd81374df76bf5c3b6
QDestTM <dimag4681@gmail.com>
2026-02-27 11:50:00 +0200

    feat(iwkits-api): update GetOrders endpoint - Implement sort definition builder - Define in dedicated namespace

--- backend/src/IWKits.Api/Features/GetOrders/GetOrdersRespond.cs
+++ backend/src/IWKits.Api/Features/GetOrders/GetOrdersRespond.cs
@@ -1,8 +1,9 @@
-namespace IWKits.Api;
+namespace IWKits.Api.Features.GetOrders;
 
 // Namespaces used by this file
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
+using IWKits.Api.Entities;
 
 // Main content of the file
 public sealed record GetOrdersRespond
